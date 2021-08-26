7cc1d6bad4517a371c227082a4fcde3c7bd66fe9
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2021-08-26 16:07:53 -0300

    configuração API gateway zuul

---
+++ hr-api-gateway-zuul/src/test/java/com/estudos/microsservicos/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.microsservicos.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
