11fa4da8cff4fb892b31cd2cabd147b8b8dc846a
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2021-08-30 12:18:31 -0300

    adição servidor de autenticação

---
+++ hr-oauth/src/test/java/com/estudos/microsservicos/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.microsservicos.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
