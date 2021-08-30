cd87da97e861a673b1322ca501896ff7b7ef568b
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2021-08-30 14:44:27 -0300

    implementação oauth

--- hr-oauth/src/main/java/com/estudos/microsservicos/hroauth/entity/User.java
+++ hr-oauth/src/main/java/com/estudos/microsservicos/hroauth/entity/User.java
@@ -1,11 +1,17 @@
 package com.estudos.microsservicos.hroauth.entity;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class User implements Serializable{
+public class User implements UserDetails,  Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
@@ -59,6 +65,43 @@ public class User implements Serializable{
 
 	public Set<Role> getRoles() {
 		return roles;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return roles.stream()
+				.map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 	
 }
--- hr-oauth/src/main/java/com/estudos/microsservicos/hroauth/service/UserService.java
+++ hr-oauth/src/main/java/com/estudos/microsservicos/hroauth/service/UserService.java
@@ -1,13 +1,16 @@
 package com.estudos.microsservicos.hroauth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.estudos.microsservicos.hroauth.entity.User;
 import com.estudos.microsservicos.hroauth.feignClients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -18,5 +21,16 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found !");
 		}
 		return us;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		User us = userFeignClient.findByEmail(username).getBody();
+		if(us == null) {
+			throw new UsernameNotFoundException("Username not found !");
+		}
+		return us;
+	
 	}
 }
