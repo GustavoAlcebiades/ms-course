fcfc85c972667659e864c13c725331028864b629
GustavoAlcebiades <gustavo.alcebiades@hotmail.com.br>
2021-08-30 10:46:11 -0300

    implementação projeto hr-user

---
+++ hr-user/src/main/java/com/estudos/microsservicos/hruser/entity/Role.java
@@ -0,0 +1,46 @@
+package com.estudos.microsservicos.hruser.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+	public Role() {}
+
+	public Role(Long id, String roleName) {
+		super();
+		this.id = id;
+		this.roleName = roleName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+	
+	
+}
